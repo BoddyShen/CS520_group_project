ac00bdfce02d50346818c63270e77fc46081e665
Boddy Shen <pohengshen96@gmail.com>
2023-10-13 23:07:08 -0400

    Feat: Add form in register page

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import React from 'react';
 import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 import {
   Card,
   CardContent,
@@ -14,7 +15,50 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
-export default function Login() {
+export default function Register() {
+  const { register, getValues, handleSubmit, formState, clearErrors } =
+    useForm();
+  const { errors } = formState;
+
+  const fields = {
+    email: register('email', { required: 'Email is required thus' }),
+    password: register('password', {
+      required: 'Password is required',
+      minLength: {
+        value: 8,
+        message: 'Password must be at least 8 characters',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Password must be less than 20 characters',
+      },
+    }),
+    confirmPassword: register('confirmPassword', {
+      required: 'Confirm Password is required',
+      minLength: {
+        value: 8,
+        message: 'Confirm Password must be at least 8 characters',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Confirm Password must be less than 20 characters',
+      },
+      validate: (value) => {
+        const passwordValue = getValues('password');
+        if (value !== passwordValue) {
+          return 'Passwords do not match';
+        }
+      },
+    }),
+  };
+
+  async function onSubmit(user: any) {
+    if (user.password !== user.confirmPassword) {
+      console.log('Password must be the same.');
+    }
+    await console.log('Form data submitted:', user);
+  }
+
   return (
     <div className="container mx-auto py-32">
       <Card>
@@ -24,37 +68,64 @@ export default function Login() {
             Enter your email below to create your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid grid-cols-1 gap-6">
-            <Button variant="outline">
-              <Icons.google className="mr-2 h-4 w-4" />
-              Google
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardContent className="grid gap-4">
+            <div className="grid grid-cols-1 gap-6">
+              <Button variant="outline">
+                <Icons.google className="mr-2 h-4 w-4" />
+                Google
+              </Button>
+            </div>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                {...fields.email}
+                type="email"
+                placeholder="ID@umass.edu"
+                onChange={() => clearErrors()}
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                {...fields.password}
+                type="password"
+                onChange={() => clearErrors()}
+              />
+              <div className="text-red-700 font-bold">
+                {errors.password?.message?.toString()}
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="confirmPassword">Confirm Password Again</Label>
+              <Input
+                {...fields.confirmPassword}
+                type="password"
+                onChange={() => clearErrors()}
+              />
+              <div className="text-red-700 font-bold">
+                {errors.confirmPassword?.message?.toString()}
+              </div>
+            </div>
+            <Button
+              type="submit"
+              className="mx-auto place-self-center w-1/2"
+              variant={'umass'}
+            >
+              Create account
             </Button>
-          </div>
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-            </div>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="NetID@umass.edu" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="mx-auto place-self-center w-1/2" variant={'umass'}>
-            Create account
-          </Button>
-        </CardFooter>
+          </CardContent>
+        </form>
       </Card>
     </div>
   );
